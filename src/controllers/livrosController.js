c562456f472f82f503388f04f99356e2813753ac
fabrizioabreu <fabrizioabreu@live.com>
2023-04-29 20:06:58 -0300

    Criando consulta por campos específico 'editora'

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -56,4 +56,12 @@ export class LivroController {
             }
         })
     }
+
+    static listarLivroPorEditora = (req, res) => {
+        const editora = req.query.editora;
+
+        livros.find({'editora': editora}, {}, (err, livros) => {
+            res.status(200).send(livros);
+        })
+    }
 }
